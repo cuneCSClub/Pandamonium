683cec96e28bcc31e2d9b05ec5e0fb3be0de017e
Joel Ristvedt <joel.ristvedt@cune.org>
2016-02-23 23:10:00 -0600

    First draft of animal spawning

--- main.lua
+++ main.lua
@@ -2,16 +2,16 @@
 function love.conf(t)
 	t.title = "Pandamonium" --Title in the window
 	t.version = "0.10.0"    --LOVE version this game was made for
-	
+
 	t.window.width = 480    --screen width
 	t.window.height = 700   --screen height
-	
+
 	-- For Windows debugging
 	t.console = true
 end
 
 function love.load()
-	
+
 	--Initialize trampoline object
 	tramp_w = 100
 	tramp_h = 20
@@ -22,12 +22,30 @@ function love.load()
 					u = 1,                                        --u vector (x axis)
 					v = 0,                                        --v vector (y axis)
 					speed = 250,                                  --vector magnitude (movement speed)
-					color = {r=255,g=255,b=255}}                  --color of trampoline
+					color = {r=255,g=0,b=255}}                  --color of trampoline
+
+ -- table that contain animal data
+ animals = {}
+ i = 1
+ for i = 1, 2000 do
+	 animals[i].x = 0
+ end
+ timer = 0
+ -- vsync goes here
 
 end
 
 
 function love.update(dt)
+
+  timer = timer + dt
+
+  if timer % 5 == 0 then
+    random_x_position = math.random(0, love.graphics.getWidth() - trampoline.width / 3)
+		animals[i].x = random_x_position
+		i = i + 1
+	  timer = 0
+	end
 
 	--Set controls for trampoline
 	if love.keyboard.isDown('a', 'left') then
@@ -49,6 +67,9 @@ end
 
 
 function love.draw()
+	for j = 1, i, 1 do
+	   love.graphics.rectangle("fill", animals[j].x, 0, 20, 20)
+	end
 	--Draw trampoline
 	love.graphics.setColor(trampoline.color.r, trampoline.color.g, trampoline.color.b)
 	love.graphics.rectangle("fill", trampoline.x, trampoline.y, trampoline.width, trampoline.height)
